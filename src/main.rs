3da981b60cbcd0938ba8eec50196b30d68879a0e
Arnav Borborah <arnavborborah11@gmail.com>
2018-12-09 18:20:49 +0000

    Add tests + deployments on Windows

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,11 @@ fn main() {
     println!("Hello, world!");
     println!("More substantial change.");
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn random_test() {
+        assert_eq!(2 + 2, 4);
+    }
+}
